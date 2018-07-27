720973f06fbd0112a408ddda33bffafea12f5619
Joes Lie <liejoes1@gmail.com>
2018-07-27 10:53:42 +0800

    Added link functionality

--- OnTime/Home.Designer.cs
+++ OnTime/Home.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.tb_intake_code = new System.Windows.Forms.TextBox();
+            this.lbl_logo = new System.Windows.Forms.Label();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.link_lbl_go = new System.Windows.Forms.LinkLabel();
+            this.lbl_message = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tb_intake_code
@@ -36,18 +40,72 @@
             this.tb_intake_code.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.tb_intake_code.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.tb_intake_code.BackColor = System.Drawing.Color.White;
-            this.tb_intake_code.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_intake_code.Location = new System.Drawing.Point(499, 275);
+            this.tb_intake_code.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_intake_code.ForeColor = System.Drawing.Color.Black;
+            this.tb_intake_code.Location = new System.Drawing.Point(101, 252);
             this.tb_intake_code.Name = "tb_intake_code";
-            this.tb_intake_code.Size = new System.Drawing.Size(429, 49);
+            this.tb_intake_code.Size = new System.Drawing.Size(264, 39);
             this.tb_intake_code.TabIndex = 2;
+            // 
+            // lbl_logo
+            // 
+            this.lbl_logo.AutoSize = true;
+            this.lbl_logo.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_logo.ForeColor = System.Drawing.Color.White;
+            this.lbl_logo.Location = new System.Drawing.Point(91, 109);
+            this.lbl_logo.Name = "lbl_logo";
+            this.lbl_logo.Size = new System.Drawing.Size(194, 65);
+            this.lbl_logo.TabIndex = 3;
+            this.lbl_logo.Text = "OnTime";
+            this.lbl_logo.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lbl_description
+            // 
+            this.lbl_description.AutoSize = true;
+            this.lbl_description.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_description.ForeColor = System.Drawing.Color.White;
+            this.lbl_description.Location = new System.Drawing.Point(97, 213);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(212, 21);
+            this.lbl_description.TabIndex = 4;
+            this.lbl_description.Text = "Please enter intake code here";
+            this.lbl_description.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // link_lbl_go
+            // 
+            this.link_lbl_go.ActiveLinkColor = System.Drawing.Color.DeepSkyBlue;
+            this.link_lbl_go.AutoSize = true;
+            this.link_lbl_go.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link_lbl_go.LinkColor = System.Drawing.Color.White;
+            this.link_lbl_go.Location = new System.Drawing.Point(371, 266);
+            this.link_lbl_go.Name = "link_lbl_go";
+            this.link_lbl_go.Size = new System.Drawing.Size(69, 25);
+            this.link_lbl_go.TabIndex = 5;
+            this.link_lbl_go.TabStop = true;
+            this.link_lbl_go.Text = "Search";
+            this.link_lbl_go.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_lbl_go_LinkClicked);
+            // 
+            // lbl_message
+            // 
+            this.lbl_message.AutoSize = true;
+            this.lbl_message.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_message.ForeColor = System.Drawing.Color.Aquamarine;
+            this.lbl_message.Location = new System.Drawing.Point(98, 315);
+            this.lbl_message.Name = "lbl_message";
+            this.lbl_message.Size = new System.Drawing.Size(0, 21);
+            this.lbl_message.TabIndex = 6;
+            this.lbl_message.Click += new System.EventHandler(this.lbl_message_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lbl_message);
+            this.Controls.Add(this.link_lbl_go);
+            this.Controls.Add(this.lbl_description);
+            this.Controls.Add(this.lbl_logo);
             this.Controls.Add(this.tb_intake_code);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -61,5 +119,9 @@
 
         #endregion
         private System.Windows.Forms.TextBox tb_intake_code;
+        private System.Windows.Forms.Label lbl_logo;
+        private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.LinkLabel link_lbl_go;
+        private System.Windows.Forms.Label lbl_message;
     }
 }
--- OnTime/Home.cs
+++ OnTime/Home.cs
@@ -24,11 +24,13 @@ namespace OnTime
             IntakeListActivity intakeListActivity = new IntakeListActivity();
             //label1.Text = intakeListActivity.GetWeek();
 
-            tb_intake_code.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            tb_intake_code.AutoCompleteMode = AutoCompleteMode.Suggest;
             tb_intake_code.AutoCompleteSource = AutoCompleteSource.CustomSource;
             var autoComplete = new AutoCompleteStringCollection();
             autoComplete.AddRange(intakeListActivity.GetIntakeCode().ToArray());
             tb_intake_code.AutoCompleteCustomSource = autoComplete;
+
+            link_lbl_go.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -40,6 +42,43 @@ namespace OnTime
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void link_lbl_go_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            IntakeListActivity intakeListActivity = new IntakeListActivity();
+            string result = tb_intake_code.Text;
+            bool status = false;
+
+            for (int i = 0; i < intakeListActivity.GetIntakeCode().Count; i++)
+            {
+                if (result == intakeListActivity.GetIntakeCode()[i])
+                {
+                    status = true;
+                    lbl_message.Text = "Success";
+                }
+            }
+
+            if (!status)
+            {
+                lbl_message.Text = "Invalid Intake Code";
+            }
+
+        }
+
+        private void lbl_message_Click(object sender, EventArgs e)
         {
 
         }
